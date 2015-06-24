3ec062bb241117b34578188bb24b174db93814a2
AL39FF13 <nina141914@gmail.com>
2015-06-24 12:39:16 -0500

    ultimo

--- js/acciones.js
+++ js/acciones.js
@@ -1,14 +1,15 @@
 // JavaScript Document
 $(document).ready(function(e) {
 document.addEventListener("deviceready",function(){
-  $('#B_Vibrar').tap(function(){
-    navigator.notification.vibrate(500);
-  });
-   $('#B_Beep').tap(function(){
-    navigator.notification.beep(1);
-  });
- },false); //deviceready
+	$('#menu').on('tap',function(){
+	navigator.vibrate(1000);
+	
+	
+});
 }); 
+});
+
+
 
 
 
